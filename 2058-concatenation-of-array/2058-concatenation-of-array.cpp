a9f80be725911c989a74ef2c690e8c1e0ea5b518
DESHMUKH AANISH RAJENDRA <101972393+anishdeshmukh9@users.noreply.github.com>
2025-10-08 22:52:33 +0530

    Time: 0 ms (100%), Space: 17.2 MB (15.88%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        int n =  nums.size();
+
+        nums.resize(n+n); 
+
+
+        for(int i=0; i<n;  i++){
+
+            nums[i+n] = nums[i];
+        }
+
+
+
+
+
+        return nums;
+
+        
+        
+    }
+};
